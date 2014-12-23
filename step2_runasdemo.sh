d82bca710f953068a6cdb4868695b36e1e1ea756
Ali Bajwa <abajwa@hortonworks.com>
2014-12-23 00:07:27 -0800

    update JAVA_HOME for 2.2

--- step2_runasdemo.sh
+++ step2_runasdemo.sh
@@ -19,21 +19,22 @@ then
 	export HADOOP_HOME=/usr/hdp/2.2.0.0-2041/hadoop
 	export HADOOP_VERSION=2.6.0.2.2.0.0-2041
 	export HDP_VERSION=2.2
+	export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.71.x86_64
 else
 	export HADOOP_HOME=/usr/lib/hadoop
+	export JDK_VER=`ls /usr/jdk64/`
+	export JAVA_HOME=/usr/jdk64/$JDK_VER	
 	export HDP_VERSION=2.1
 fi
 
 export PYTHONPATH=/usr/lib/python2.6/site-packages
-export JDK_VER=`ls /usr/jdk64/`
-export JAVA_HOME=/usr/jdk64/$JDK_VER
 export YARN_CONF_DIR=/etc/hadoop/conf
 export HADOOP_CONF_DIR=/etc/hadoop/conf
 
 echo "export HADOOP_HOME=$HADOOP_HOME" >> $HOME_DIR/.bashrc
 echo "export HADOOP_VERSION=$HADOOP_VERSION" >> $HOME_DIR/.bashrc
 echo "export PYTHONPATH=$PYTHONPATH" >> $HOME_DIR/.bashrc
-echo "export JAVA_HOME=/usr/jdk64/$JDK_VER"  >> $HOME_DIR/.bashrc 
+echo "export JAVA_HOME=$JAVA_HOME"  >> $HOME_DIR/.bashrc 
 echo "export YARN_CONF_DIR=/etc/hadoop/conf" >> ~/.bashrc
 echo "export HADOOP_CONF_DIR=/etc/hadoop/conf" >> ~/.bashrc
 
