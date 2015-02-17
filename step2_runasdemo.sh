b01ed07c5e38c6f6d3b002171e5efe0de3886bdd
Ali Bajwa <abajwa@hortonworks.com>
2015-02-17 00:37:24 -0800

    removed incorrect git

--- step2_runasdemo.sh
+++ step2_runasdemo.sh
@@ -224,7 +224,6 @@ echo "execfile(os.path.join(spark_home, 'python/pyspark/shell.py'))" >> $HOME_DI
 
 
 #Install scalding for R demo
-git clone scalding
 git clone https://github.com/twitter/scalding.git
 cd scalding/
 git checkout master
